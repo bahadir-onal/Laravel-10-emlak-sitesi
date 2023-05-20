c3fbd64c631e966ddbc6db6945b597e49df75a17
Bahadir Onal <bahadironal3@gmail.com>
2023-05-20 15:34:39 +0300

    parçalama işlemi ve çıkış işlemi yapıldı

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ require __DIR__.'/auth.php';
     Route::middleware(['auth', 'role:admin'])->group(function () { 
 
         Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+        Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 
     });
 
