f1be1bec7c923af8a58a416e9deaebaf525486cb
Bahadir Onal <bahadironal3@gmail.com>
2023-05-22 14:30:51 +0300

    kullanıcı login ve register sayfası güncellendi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,121 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('frontend.frontend_dashboard')
+@section('main')
+        <!--Page Title-->
+        <section class="page-title-two bg-color-1 centred">
+            <div class="pattern-layer">
+                <div class="pattern-1" style="background-image: url({{ asset('frontend/assets/images/shape/shape-9.png') }});"></div>
+                <div class="pattern-2" style="background-image: url({{ asset('frontend/assets/images/shape/shape-10.png') }});"></div>
+            </div>
+            <div class="auto-container">
+                <div class="content-box clearfix">
+                    <h1>Sign In</h1>
+                    <ul class="bread-crumb clearfix">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Sign In</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!--End Page Title-->
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+        
+        <!-- ragister-section -->
+        <section class="ragister-section centred sec-pad">
+            <div class="auto-container">
+                <div class="row clearfix">
+                    <div class="col-xl-8 col-lg-12 col-md-12 offset-xl-2 big-column">
+                        <div class="sec-title">
+                            <h5>Sign in</h5>
+                            <h2>Sign In With Realshed</h2>
+                        </div>
+                        <div class="tabs-box">
+                            <div class="tab-btn-box">
+                                <ul class="tab-btns tab-buttons centred clearfix">
+                                    <li class="tab-btn active-btn" data-tab="#tab-1">Login</li>
+                                    <li class="tab-btn" data-tab="#tab-2">Register</li>
+                                </ul>
+                            </div>
+                            <div class="tabs-content">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="login" :value="__('Email/Name/Phone')" />
-            <x-text-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
-        </div>
+                                <div class="tab active-tab" id="tab-1">
+                                    <div class="inner-box">
+                                        <h4>Login</h4>
+                                        <form action="{{ route('login') }}" method="post" class="default-form">
+                                        @csrf
+                                            <div class="form-group">
+                                                <label>Email/Name/Phone</label>
+                                                <input type="text" id="login" name="login" required="">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Password</label>
+                                                <input type="password" name="password" id="password" required="">
+                                            </div>
+                                            <div class="form-group message-btn">
+                                                <button type="submit" class="theme-btn btn-one">Sign in</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                                <div class="tab" id="tab-2">
+                                    <div class="inner-box">
+                                        <h4>Register</h4>
+                                        <form action="{{ route('register') }}" method="post" class="default-form">
+                                        @csrf
+                                            <div class="form-group">
+                                                <label>User name</label>
+                                                <input type="text" name="name" id="name" required="">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Email address</label>
+                                                <input type="email" name="email" id="email" required="">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Password</label>
+                                                <input type="password" name="password" id="password" required="">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Confirm Password</label>
+                                                <input type="password" name="password_confirmation" id="password_confirmation" required="">
+                                            </div>
+                                            <div class="form-group message-btn">
+                                                <button type="submit" class="theme-btn btn-one">Sign in</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ragister-section end -->
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <!-- subscribe-section -->
+        <section class="subscribe-section bg-color-3">
+            <div class="pattern-layer" style="background-image: url(assets/images/shape/shape-2.png);"></div>
+            <div class="auto-container">
+                <div class="row clearfix">
+                    <div class="col-lg-6 col-md-6 col-sm-12 text-column">
+                        <div class="text">
+                            <span>Subscribe</span>
+                            <h2>Sign Up To Our Newsletter To Get The Latest News And Offers.</h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 form-column">
+                        <div class="form-inner">
+                            <form action="contact.html" method="post" class="subscribe-form">
+                                <div class="form-group">
+                                    <input type="email" name="email" placeholder="Enter your email" required="">
+                                    <button type="submit">Subscribe Now</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- subscribe-section end -->
+@endsection
