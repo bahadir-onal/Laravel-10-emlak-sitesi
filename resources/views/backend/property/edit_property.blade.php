acc3689ec932efacec859f7e7ec2958e3d1ddc71
Bahadir Onal <bahadironal3@gmail.com>
2023-05-29 11:05:18 +0300

    property facilities edit başlandı

--- resources/views/backend/property/edit_property.blade.php
+++ resources/views/backend/property/edit_property.blade.php
@@ -320,6 +320,111 @@
     </div> 
     <!--  /// End Property Multi Image Update //// -->
 
+    <!--  /// Facility Update //// -->
+    <div class="page-content" style="margin-top: -35px;" > 
+        <div class="row profile-body"> 
+            <div class="col-md-12 col-xl-12 middle-wrapper">
+                <div class="row">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6 class="card-title">Edit Property Facility </h6>
+                            <form method="post" action="{{ route('update.property.thumbnail') }}" id="myForm" enctype="multipart/form-data">
+                                
+
+                                    <div class="row add_item">
+                                        <div class="col-md-4">
+                                            <div class="mb-3">
+                                                <label for="facility_name" class="form-label">Facilities </label>
+                                                <select name="facility_name[]" id="facility_name" class="form-control">
+                                                    <option value="">Select Facility</option>
+                                                    <option value="Hospital">Hospital</option>
+                                                    <option value="SuperMarket">Super Market</option>
+                                                    <option value="School">School</option>
+                                                    <option value="Entertainment">Entertainment</option>
+                                                    <option value="Pharmacy">Pharmacy</option>
+                                                    <option value="Airport">Airport</option>
+                                                    <option value="Railways">Railways</option>
+                                                    <option value="Bus Stop">Bus Stop</option>
+                                                    <option value="Beach">Beach</option>
+                                                    <option value="Mall">Mall</option>
+                                                    <option value="Bank">Bank</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="mb-3">
+                                                <label for="distance" class="form-label"> Distance </label>
+                                                <input type="text" name="distance[]" id="distance" class="form-control" placeholder="Distance (Km)">
+                                            </div>
+                                        </div>
+                                        <div class="form-group col-md-4" style="padding-top: 30px;">
+                                            <a class="btn btn-success addeventmore"><i class="fa fa-plus-circle"></i> Add More..</a>
+                                        </div>
+                                    </div> <!---end row-->
+                            </form> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+    <!--    /// End Facility Update //// -->
+
+    <!--========== Start of add multiple class with ajax ==============-->
+    <div style="visibility: hidden">
+        <div class="whole_extra_item_add" id="whole_extra_item_add">
+            <div class="whole_extra_item_delete" id="whole_extra_item_delete">
+                <div class="container mt-2">
+                    <div class="row">
+                                            
+                    <div class="form-group col-md-4">
+                        <label for="facility_name">Facilities</label>
+                        <select name="facility_name[]" id="facility_name" class="form-control">
+                                <option value="">Select Facility</option>
+                                <option value="Hospital">Hospital</option>
+                                <option value="SuperMarket">Super Market</option>
+                                <option value="School">School</option>
+                                <option value="Entertainment">Entertainment</option>
+                                <option value="Pharmacy">Pharmacy</option>
+                                <option value="Airport">Airport</option>
+                                <option value="Railways">Railways</option>
+                                <option value="Bus Stop">Bus Stop</option>
+                                <option value="Beach">Beach</option>
+                                <option value="Mall">Mall</option>
+                                <option value="Bank">Bank</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="distance">Distance</label>
+                        <input type="text" name="distance[]" id="distance" class="form-control" placeholder="Distance (Km)">
+                    </div>
+                    <div class="form-group col-md-4" style="padding-top: 20px">
+                        <span class="btn btn-success btn-sm addeventmore"><i class="fa fa-plus-circle">Add</i></span>
+                        <span class="btn btn-danger btn-sm removeeventmore"><i class="fa fa-minus-circle">Remove</i></span>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
+    
+     <!----For Section-------->
+     <script type="text/javascript">
+        $(document).ready(function(){
+        var counter = 0;
+        $(document).on("click",".addeventmore",function(){
+                var whole_extra_item_add = $("#whole_extra_item_add").html();
+                $(this).closest(".add_item").append(whole_extra_item_add);
+                counter++;
+        });
+        $(document).on("click",".removeeventmore",function(event){
+                $(this).closest("#whole_extra_item_delete").remove();
+                counter -= 1
+        });
+    });
+    </script>
+    <!--========== End of add multiple class with ajax ==============-->
+
     
     <script type="text/javascript">
     $(document).ready(function (){
