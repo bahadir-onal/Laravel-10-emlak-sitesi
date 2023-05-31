ec1714a8c4971c5c3bcfb4f0a9a5c3b4e86ce8a6
Bahadir Onal <bahadironal3@gmail.com>
2023-05-31 12:47:49 +0300

    middleware hatası düzeltildi

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AgentController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\PropertyTypeController;
 use App\Http\Controllers\Backend\PropertyController;
-
+use App\Http\Middleware\RedirectIfAuthenticated;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -62,7 +62,7 @@ require __DIR__.'/auth.php';
     });
 
 
-    Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+    Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login')->middleware(RedirectIfAuthenticated::class);
 
     // ADMIN GROUP MİDDLEWARE
     Route::middleware(['auth', 'role:admin'])->group(function () { 
