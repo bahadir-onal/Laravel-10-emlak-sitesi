62bcb97c7106f51a0512ff139ccf53118364afdd
Bahadir Onal <bahadironal3@gmail.com>
2023-06-13 14:48:46 +0300

    agent profil güncelleme yapıldı

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,8 @@ require __DIR__.'/auth.php';
 
         Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
         Route::get('/agent/logout', [AgentController::class, 'AgentLogout'])->name('agent.logout');
+        Route::get('/agent/profile', [AgentController::class, 'AgentProfile'])->name('agent.profile');
+        Route::post('/agent/profile/store', [AgentController::class, 'AgentProfileStore'])->name('agent.profile.store');
 
     });
 
